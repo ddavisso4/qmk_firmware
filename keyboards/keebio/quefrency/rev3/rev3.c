98839b5371342553b37f0a7c92d8caf7c325df77
ddavisso4 <daniel.davisson44@gmail.com>
2023-10-19 20:52:57 -0700

    Fix CAPS lock and add to keymap

--- keyboards/keebio/quefrency/rev3/rev3.c
+++ keyboards/keebio/quefrency/rev3/rev3.c
@@ -17,20 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 #include "split_util.h"
 
-void matrix_init_kb(void) {
-    // Disable permanent CAPS LOCK LED until I can figure out how to make it work
-    // setPinOutput(CAPS_LOCK_LED_PIN);
-    matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    // Only update if left half
-    if (led_update_user(led_state) && isLeftHand) {
-        writePin(CAPS_LOCK_LED_PIN, !led_state.caps_lock);
-    }
-    return true;
-}
-
 void eeconfig_init_kb(void) {
 #ifdef BACKLIGHT_ENABLE
     backlight_enable();
