a5de7baf686ff3319d564aedb3a27f9c4ee4f9a6
ddavisso4 <daniel.davisson44@gmail.com>
2024-02-10 15:00:49 -0800

    Custom layer locking and get rid of TT which has unusable delay

--- keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
@@ -23,7 +23,10 @@ enum custom_keycodes {
     MC_UNCMNT,
     MC_GO_BASE,
     MC_NAV_S_UP,
-    MC_NAV_S_DWN
+    MC_NAV_S_DWN,
+    LAYER_LOCK_NAV,
+    LAYER_LOCK_FN1,
+    LAYER_LOCK_FN2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -35,23 +38,23 @@ KC_TAB   ,KC_Q      ,KC_W      ,KC_E    ,KC_R    ,KC_T                ,         
             KC_NO, KC_NO, KC_NO,
 KC_CAPS  ,KC_A      ,KC_S      ,KC_D    ,KC_F    ,KC_G                ,         KC_H     ,KC_J     ,KC_K     ,KC_L     ,KC_SCLN  ,KC_QUOT    ,KC_ENTER  ,KC_ENTER ,
             KC_NO, KC_NO, KC_NO,
-KC_LSFT  ,KC_BSLS   ,KC_Z      ,KC_X    ,KC_C    ,KC_V    ,KC_B       ,         KC_N     ,KC_M     ,KC_COMM  ,KC_DOT   ,KC_SLSH  ,KC_RSFT    ,TT(UTIL)  ,
+KC_LSFT  ,KC_BSLS   ,KC_Z      ,KC_X    ,KC_C    ,KC_V    ,KC_B       ,         KC_N     ,KC_M     ,KC_COMM  ,KC_DOT   ,KC_SLSH  ,KC_RSFT    ,MO(UTIL)  ,
             KC_NO, KC_NO, KC_NO,
-KC_LCTL  ,KC_LGUI   ,KC_LALT   ,TT(FN2) ,TT(FN1) ,TT(NAV)             ,         KC_SPC   ,KC_SPC   ,KC_RALT  ,KC_RGUI  ,KC_RCTL  ,RCS(KC_M)  ,G(KC_L)   ,
+KC_LCTL  ,KC_LGUI   ,KC_LALT   ,MO(FN2) ,MO(FN1) ,MO(NAV)             ,         KC_SPC   ,KC_SPC   ,KC_RALT  ,KC_RGUI  ,KC_RCTL  ,RCS(KC_M)  ,G(KC_L)   ,
             KC_NO),
 
 
 	[NAV] = LAYOUT_all(
             KC_NO, KC_NO,
-MC_ADMIN   ,LT(0,C(KC_F4))   ,LT(0,A(KC_TAB))  ,LT(-1,C(KC_TAB))      ,LT(0,C(KC_T))   ,LT(0,G(C(KC_LEFT)))   ,LT(0,G(C(KC_RIGHT)))       ,       KC_PGUP         ,LT(1, MC_NAV_S_UP) ,KC_NO               ,KC_NO          ,KC_NO          ,KC_NO      ,KC_NO          ,KC_NO          ,
+LT(0,MC_ADMIN)  ,LT(0,C(KC_F4))   ,LT(0,A(KC_TAB))  ,LT(-1,C(KC_TAB))  ,LT(0,C(KC_T))   ,LT(0,G(C(KC_LEFT)))   ,LT(0,G(C(KC_RIGHT)))       ,       KC_PGUP         ,LT(1, MC_NAV_S_UP) ,KC_NO               ,KC_NO          ,KC_NO          ,KC_NO      ,KC_NO          ,KC_NO          ,
             KC_NO, KC_NO, KC_NO,
-RCS(KC_L)  ,LT(-2,KC_DEL)    ,LT(-1,KC_DEL)    ,LT(0,KC_UP)           ,LT(0,KC_BSPC)   ,C(KC_BSPC)                                        ,       A(KC_UP)        ,KC_PGDN            ,S(KC_UP)            ,C(KC_A)        ,C(KC_W)        ,KC_NO      ,KC_NO          ,KC_NO          ,
+RCS(KC_L)       ,LT(-2,KC_DEL)    ,LT(-1,KC_DEL)    ,LT(0,KC_UP)       ,LT(0,KC_BSPC)   ,C(KC_BSPC)                                        ,       A(KC_UP)        ,KC_PGDN            ,S(KC_UP)            ,C(KC_A)        ,C(KC_W)        ,KC_NO      ,KC_NO          ,KC_NO          ,
             KC_NO, KC_NO, KC_NO,
-KC_ESC     ,C(KC_LEFT)       ,LT(-1,KC_LEFT)   ,LT(0,KC_DOWN)         ,LT(-1,KC_RGHT)  ,C(KC_RGHT)                                        ,       RCS(KC_LEFT)    ,S(KC_LEFT)         ,S(KC_DOWN)          ,S(KC_RGHT)     ,RCS(KC_RGHT)   ,KC_NO      ,C(KC_ENTER)    ,C(KC_ENTER)    ,
+KC_ESC          ,C(KC_LEFT)       ,LT(-1,KC_LEFT)   ,LT(0,KC_DOWN)     ,LT(-1,KC_RGHT)  ,C(KC_RGHT)                                        ,       RCS(KC_LEFT)    ,S(KC_LEFT)         ,S(KC_DOWN)          ,S(KC_RGHT)     ,RCS(KC_RGHT)   ,KC_NO      ,C(KC_ENTER)    ,C(KC_ENTER)    ,
             KC_NO, KC_NO, KC_NO,
-KC_ENTER   ,KC_NO            ,LT(0, KC_HOME)   ,LT(0,C(KC_X))         ,LT(0,C(KC_C))  ,LT(0,C(KC_V))          ,LT(0, KC_END)              ,       A(KC_DOWN)      ,LT(-1,KC_HOME)     ,LT(0, MC_NAV_S_DWN) ,LT(-1,KC_END)  ,KC_NO          ,KC_NO      ,KC_NO          ,
+KC_ENTER        ,KC_NO            ,LT(0, KC_HOME)   ,LT(0,C(KC_X))     ,LT(0,C(KC_C))  ,LT(0,C(KC_V))          ,LT(0, KC_END)              ,       A(KC_DOWN)      ,LT(-1,KC_HOME)     ,LT(0, MC_NAV_S_DWN) ,LT(-1,KC_END)  ,KC_NO          ,KC_NO      ,KC_NO          ,
             KC_NO, KC_NO, KC_NO,
-KC_SPACE   ,KC_TRNS          ,KC_TRNS          ,LT(0,KC_PRINT_SCREEN) ,KC_F5          ,KC_TRNS                                            ,       C(KC_SPACE)     ,C(KC_SPACE)        ,KC_TRNS             ,KC_TRNS        ,KC_TRNS        ,KC_TRNS    ,KC_TRNS        ,
+KC_SPACE        ,KC_APPLICATION   ,KC_PRINT_SCREEN  ,C(KC_R)           ,KC_F5          ,KC_TRNS                                            ,       C(KC_SPACE)     ,C(KC_SPACE)        ,KC_TRNS             ,KC_TRNS        ,KC_TRNS        ,KC_TRNS    ,KC_TRNS        ,
         KC_NO),
 
 
@@ -85,15 +88,15 @@ KC_NO       ,KC_NO      ,KC_NO      ,KC_TRNS   ,KC_NO     ,KC_TRNS              
 
 	[UTIL] = LAYOUT_all(
             KC_NO, KC_NO,
-KC_NO   ,KC_F1     ,KC_F2     ,KC_F3    ,KC_F4     ,KC_F5   ,KC_F6         ,        KC_F7   ,KC_F8                         ,KC_F9                         ,KC_F10                     ,KC_F11   ,KC_F12    ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_F1     ,KC_F2     ,KC_F3    ,KC_F4     ,KC_F5   ,KC_F6                    ,        KC_F7   ,KC_F8                         ,KC_F9                         ,KC_F10                     ,KC_F11   ,KC_F12    ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_INS    ,KC_PAUS   ,KC_SCRL  ,KC_NUM    ,KC_NO                  ,        KC_NO   ,QK_DYNAMIC_TAPPING_TERM_PRINT , QK_DYNAMIC_TAPPING_TERM_DOWN ,QK_DYNAMIC_TAPPING_TERM_UP ,KC_NO    ,KC_NO     ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_INS    ,KC_PAUS   ,KC_SCRL  ,KC_NUM    ,KC_NO                             ,        KC_NO   ,QK_DYNAMIC_TAPPING_TERM_PRINT , QK_DYNAMIC_TAPPING_TERM_DOWN ,QK_DYNAMIC_TAPPING_TERM_UP ,KC_NO    ,KC_NO     ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,BL_TOGG   ,BL_DOWN   ,BL_UP    ,KC_NO     ,KC_NO                  ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_NO     ,KC_NO   ,KC_NO   ,
+KC_NO   ,BL_TOGG   ,BL_DOWN   ,BL_UP    ,KC_NO     ,KC_NO                             ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_NO     ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO     ,RGB_TOG   ,RGB_VAD  ,RGB_VAI   ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_NO     ,KC_NO   ,
+KC_NO   ,KC_NO     ,RGB_TOG   ,RGB_VAD        ,RGB_VAI        ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_NO     ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO     ,KC_NO     ,KC_NO    ,KC_NO     ,KC_NO                  ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_TRNS   ,KC_NO   ,
+KC_NO   ,KC_NO     ,KC_NO     ,LAYER_LOCK_FN2 ,LAYER_LOCK_FN1 ,LAYER_LOCK_NAV         ,        KC_NO   ,KC_NO                         ,KC_NO                         ,KC_NO                      ,KC_NO    ,KC_TRNS   ,KC_NO   ,
             KC_NO),
 
 
@@ -110,7 +113,6 @@ KC_NO   ,KC_NO     ,KC_NO           ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO         , 
 KC_NO   ,KC_NO     ,KC_NO           ,KC_NO   ,KC_NO   ,MC_CLR_ALT_TAB         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO),
 
-    // Blank layer
     [WIN_TAB_SWITCH] = LAYOUT_all(
             KC_NO, KC_NO,
 KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,MC_GO_BASE ,
@@ -155,9 +157,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 
     switch (keycode) {
+
         // Send string macros
-        case MC_ADMIN:
-            if (record->event.pressed) {
+        case LT(0,MC_ADMIN):
+            if (record->tap.count && record->event.pressed) {
+                SEND_STRING(
+                    SS_TAP(X_TAB)
+                    SS_DELAY(25)
+                    SS_TAP(X_TAB)
+                    SS_DELAY(25)
+                    SS_TAP(X_ENTER)
+                    SS_DELAY(100)
+                    SS_TAP(X_TAB)
+                    SS_DELAY(25)
+                    SS_TAP(X_TAB)
+                    SS_DELAY(25)
+                    SS_TAP(X_ENTER)
+                    SS_DELAY(50)
+                    SS_TAP(X_TAB)
+                    SS_DELAY(25)
+                    LOCAL_ADMIN_ACCOUNT
+                    SS_DELAY(25)
+                    SS_DOWN(X_LSFT) SS_TAP(X_TAB) SS_UP(X_LSFT));
+            } else if (record->event.pressed) {
                 SEND_STRING(
                     SS_TAP(X_TAB)
                     SS_DELAY(25)
@@ -176,9 +198,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     ADMIN_ACCOUNT
                     SS_DELAY(25)
                     SS_DOWN(X_LSFT) SS_TAP(X_TAB) SS_UP(X_LSFT));
-                return false;
             }
-            return true;
+            return false;
         case MC_TG_BKMK:
             if (record->event.pressed) {
                 SEND_STRING_DELAY(SS_LCTL("kk"), 20);
@@ -195,10 +216,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_LGUI);
             reset_to_base();
             return false;
-        case TT(NAV):
+        case MO(NAV):
             // Special case where pressed on base layer and released on win tab layer
             if(IS_LAYER_ON(WIN_TAB_SWITCH) && !record->event.pressed) {
                 unregister_code(KC_LGUI);
+                reset_to_base();
+                return false;
+            }
+            // Already on NAV and pressed means we are in lock mode and want to return to BASE
+            else if(IS_LAYER_ON(NAV) && record->event.pressed) {
+                reset_to_base();
+                return false;
+            }
+            return true;
+        case MO(FN1):
+            if(IS_LAYER_ON(FN1) && record->event.pressed) {
+                reset_to_base();
+                return false;
+            }
+            return true;
+        case MO(FN2):
+            if(IS_LAYER_ON(FN2) && record->event.pressed) {
                 reset_to_base();
                 return false;
             }
@@ -221,6 +259,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             return true;
+        case LAYER_LOCK_NAV:
+            layer_move(NAV);
+            return false;
+        case LAYER_LOCK_FN1:
+            layer_move(FN1);
+            return false;
+        case LAYER_LOCK_FN2:
+            layer_move(FN2);
+            return false;
 
         // Tap-Hold Hold Overrides
         case LT(0, KC_HOME):
@@ -409,13 +456,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code16(KC_F11); // Tap
             } else if (record->event.pressed) {
                 tap_code16(C(KC_E));
-            }
-            return false;
-        case LT(0,KC_PRINT_SCREEN):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(C(KC_R)); // Tap
-            } else if (record->event.pressed) {
-                tap_code16(KC_PRINT_SCREEN);
             }
             return false;
         case LT(-2,KC_DEL):
