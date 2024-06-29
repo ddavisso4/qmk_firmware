51c59adb183e6c440795303c5b429567ee7929d2
ddavisso4 <daniel.davisson44@gmail.com>
2024-06-29 15:39:19 -0700

    Post rebase cleanup

--- keyboards/keebio/quefrency/rev3/config.h
+++ keyboards/keebio/quefrency/rev3/config.h
@@ -18,4 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define CAPS_LOCK_LED_PIN B6
+#define SPLIT_HAND_PIN F7
+#define LED_CAPS_LOCK_PIN B6
+#define LED_PIN_ON_STATE 0
