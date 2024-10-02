17c15aa486fa313e3ea02b37edd8cc79118a8ca8
ddavisso4 <daniel.davisson44@gmail.com>
2024-10-01 22:04:09 -0700

    Debounce, one shots, brightness tweak

--- keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
@@ -68,15 +68,15 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_all(
             KC_NO, KC_NO,
-KC_GRV   ,KC_1                   ,KC_2      ,KC_3    ,KC_4    ,KC_5    ,KC_6        ,         KC_7     ,KC_8           ,KC_9          ,KC_0      ,KC_MINS                ,KC_EQL     ,KC_BSPC   ,KC_BSPC  ,
+KC_GRV   ,KC_1                   ,KC_2          ,KC_3    ,KC_4    ,KC_5    ,KC_6        ,         KC_7     ,KC_8           ,KC_9          ,KC_0      ,KC_MINS                ,KC_EQL     ,KC_BSPC   ,KC_BSPC  ,
             KC_NO, KC_NO, KC_NO,
-KC_TAB   ,KC_Q                   ,KC_W      ,KC_E    ,KC_R    ,KC_T                 ,         KC_Y     ,KC_U           ,KC_I          ,KC_O      ,KC_P                   ,KC_LBRC    ,KC_RBRC   ,KC_BSLS  ,
+KC_TAB   ,KC_Q                   ,KC_W          ,KC_E    ,KC_R    ,KC_T                 ,         KC_Y     ,KC_U           ,KC_I          ,KC_O      ,KC_P                   ,KC_LBRC    ,KC_RBRC   ,KC_BSLS  ,
             KC_NO, KC_NO, KC_NO,
-KC_CAPS  ,KC_A                   ,KC_S      ,KC_D    ,KC_F    ,KC_G                 ,         KC_H     ,KC_J           ,KC_K          ,KC_L      ,KC_SCLN                ,KC_QUOT    ,KC_ENTER  ,KC_ENTER ,
+KC_CAPS  ,KC_A                   ,KC_S          ,KC_D    ,KC_F    ,KC_G                 ,         KC_H     ,KC_J           ,KC_K          ,KC_L      ,KC_SCLN                ,KC_QUOT    ,KC_ENTER  ,KC_ENTER ,
             KC_NO, KC_NO, KC_NO,
-KC_LSFT  ,MT(MOD_LSFT, KC_BSLS)  ,KC_Z      ,KC_X    ,KC_C    ,KC_V    ,KC_B        ,         KC_N     ,KC_M           ,KC_COMM       ,KC_DOT    ,MT(MOD_RSFT, KC_SLSH)  ,KC_RSFT    ,MO(UTIL)  ,
+KC_LSFT  ,MT(MOD_LSFT, KC_BSLS)  ,KC_Z          ,KC_X    ,KC_C    ,KC_V    ,KC_B        ,         KC_N     ,KC_M           ,KC_COMM       ,KC_DOT    ,MT(MOD_RSFT, KC_SLSH)  ,KC_RSFT    ,MO(UTIL)  ,
             KC_NO, KC_NO, KC_NO,
-KC_LCTL  ,KC_LGUI                ,KC_LALT   ,MO(FN2) ,MO(FN1) ,MO(NAV)              ,         KC_SPC   ,OSL(ONE_SHOT)  ,OSM(MOD_LALT) ,RCS(KC_M) ,KC_NO                  ,G(KC_L)    ,G(KC_L)   ,
+KC_LCTL  ,KC_LGUI                ,OSM(MOD_LALT) ,MO(FN2) ,MO(FN1) ,MO(NAV)              ,         KC_SPC   ,OSL(ONE_SHOT)  ,OSM(MOD_RALT) ,RCS(KC_M) ,KC_NO                  ,G(KC_L)    ,G(KC_L)   ,
             KC_NO),
 
 	[NAV] = LAYOUT_all(
@@ -163,15 +163,15 @@ KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO                  ,        KC_
 
     [ONE_SHOT] = LAYOUT_all(
             KC_NO, KC_NO,
-KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO       ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO   ,USE_WMS ,USE_ERP ,USE_PPG     ,RCS(KC_ESC)            ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_NO   ,USE_WMS ,USE_ERP     ,USE_PPG     ,RCS(KC_ESC)            ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,PR_TEMPLATE ,KC_NO                  ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_NO   ,KC_NO   ,LCA(KC_DEL) ,PR_TEMPLATE ,KC_NO                  ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO       ,KC_NO   ,KC_NO         ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO, KC_NO, KC_NO,
-KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO                  ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO       ,KC_NO                  ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
             KC_NO)
 };
 
@@ -693,6 +693,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
 
+        // Long nav
         case RIGHT_MEDIUM:
             if (record->event.pressed) {
                 repeat(KC_RIGHT, NAV_MEDIUM);
