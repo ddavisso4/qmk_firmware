821c74a848c9ca34ba305a7bffe8a59598fd028e
Stefan Kerkmann <karlk90@pm.me>
2024-10-18 09:57:08 +0200

    [Core] `usb_device_state`: consolidate usb state handling across implementations (#24258)
    * usb_device_state: add idle_rate, led and protocol
    
    Previously all usb drivers and platform implementations (expect for our
    oddball atsam) tracked the same two global variables:
    
    - keyboard_protocol: to indicate if we are in report or boot protocol
    - keyboard_idle: for the idle_rate of the keyboard endpoint
    
    And a local variable that was exposed trough some indirection:
    
    - keyboard_led_state: for the currently set indicator leds (caps lock etc.)
    
    These have all been moved into the usb_device_state struct wich is
    accessible by getters and setters.
    
    This reduces code duplication and centralizes the state management
    across platforms and drivers.
    
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>
    
    * usb_device_state: reset protocol on reset
    
    The usb hid specification section 7.2.6 states:
    
    When initialized, all devices default to report protocol. However the
    host should not make any assumptions about the device’s state and should
    set the desired protocol whenever initializing a device.
    
    Thus on reset we should always do exactly that.
    
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>
    
    * keyboards: fix oversize warnings
    
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>
    
    ---------
    
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>

--- quantum/action_util.c
+++ quantum/action_util.c
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "action_layer.h"
 #include "timer.h"
 #include "keycode_config.h"
+#include "usb_device_state.h"
 #include <string.h>
 
 #include "wait.h"
