dcb5272370b94ccb289ec43d95b3f34ce468c979
ddavisso4 <daniel.davisson44@gmail.com>
2024-11-25 21:24:32 -0800

    Starcraft tweaks

--- keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ddavisso4/keymap.c
@@ -72,7 +72,7 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[BASE] = LAYOUT_all(
+[BASE] = LAYOUT_all(
             KC_NO, KC_NO,
 KC_GRV   ,KC_1                   ,KC_2          ,KC_3    ,KC_4    ,KC_5    ,KC_6        ,         KC_7     ,KC_8           ,KC_9          ,KC_0      ,KC_MINS                ,KC_EQL     ,KC_BSPC   ,KC_BSPC  ,
             KC_NO, KC_NO, KC_NO,
@@ -182,9 +182,9 @@ KC_NO   ,KC_NO   ,KC_NO   ,KC_NO       ,KC_NO       ,KC_NO                   ,  
 
     [STARCRAFT_PLAY] = LAYOUT_all(
             KC_NO, KC_NO,
-KC_ESCAPE           ,LT(3,KC_1) ,LT(3,KC_2) ,LT(3,KC_3) ,LT(3,KC_4) ,LT(3,KC_5) ,LT(3,KC_6)        ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,TO(BASE) ,
+TO(STARCRAFT_TYPE)  ,LT(3,KC_1) ,LT(3,KC_2) ,LT(3,KC_3) ,LT(3,KC_4) ,LT(3,KC_5) ,LT(3,KC_6)        ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,TO(BASE) ,
             KC_NO, KC_NO, KC_NO,
-LT(3,KC_F2)         ,LT(3,KC_7) ,LT(3,KC_8) ,LT(3,KC_9) ,LT(3,KC_0) ,KC_T                          ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO    ,
+LT(3,KC_F2)         ,LT(3,KC_7) ,LT(3,KC_8) ,LT(3,KC_9) ,LT(3,KC_0) ,KC_ESC                        ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO    ,
             KC_NO, KC_NO, KC_NO,
 LT(3,KC_F3)         ,KC_A       ,KC_S       ,KC_D       ,KC_SPACE   ,KC_G                          ,        KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO    ,
             KC_NO, KC_NO, KC_NO,
